293c2d589aa806a1547f2e3f62f33c27854b0e98
abiRH21 <52263861+abiRH21@users.noreply.github.com>
2019-07-28 21:36:23 -0400

    Added Kotlin code to facilitate Login, Gradle Deps

---
+++ main/java/com/example/raghura/cashnowapp/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.raghura.cashnowapp
+
+object Constants {
+    const val TAG = "MQ"
+    const val SETTINGS_COLLECTION ="settings"
+    const val  PICS_COLLECTION = "pics"
+}
--- main/java/com/example/raghura/cashnowapp/MainActivity.kt
+++ main/java/com/example/raghura/cashnowapp/MainActivity.kt
@@ -5,14 +5,102 @@ import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MyOffersListFragment.OnOfferSelectedListener  , AcceptedOffersListFragment.OnOfferSelectedListenerAccepted , AvailableOffersListFragment.OnOfferSelectedListener {
+class MainActivity : AppCompatActivity(),SplashFragment.OnLoginButtonPressedListener, MyOffersListFragment.OnOfferSelectedListener  , AcceptedOffersListFragment.OnOfferSelectedListenerAccepted , AvailableOffersListFragment.OnOfferSelectedListener {
     var Fbool : Boolean = false
+
+    //authentication stuff
+    private val auth = FirebaseAuth.getInstance()
+    lateinit var authStateListener : FirebaseAuth.AuthStateListener
+    private val RC_SIGN_IN = 1
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener (authStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener { authStateListener }
+    }
+    lateinit var Guser : FirebaseUser
+    private fun initializeListeners() {
+        // TODO: Create an AuthStateListener that passes the UID
+        // to the MovieQuoteFragment if the user is logged in
+        // and goes back to the Splash fragment otherwise.
+        // See https://firebase.google.com/docs/auth/users#the_user_lifecycle
+        authStateListener = FirebaseAuth.AuthStateListener { auth :FirebaseAuth ->
+            val user = auth.currentUser
+
+            Log.d(Constants.TAG, "In auth Listener, user = $user")
+            if(user != null) {
+                Guser= user!!
+                Log.d(Constants.TAG,"UID: ${user.uid}")
+                Log.d(Constants.TAG,"Name: ${user.displayName}")
+                Log.d(Constants.TAG,"Email: ${user.email}")
+                Log.d(Constants.TAG,"Phone: ${user.phoneNumber}")
+                Log.d(Constants.TAG,"Photo: ${user.photoUrl}")
+                //switchToMovieQuoteFragment(user.uid)
+                Toast.makeText(this, "made it ", Toast.LENGTH_LONG)
+//                var switchTo : Fragment? = null
+//                switchTo = PicFragment()
+//
+//                if  ( switchTo != null) {
+//                    val ft = supportFragmentManager.beginTransaction()
+//                    ft.replace(R.id.fragment_container, switchTo)
+//                    while (supportFragmentManager.backStackEntryCount > 0) {
+//                        supportFragmentManager.popBackStackImmediate()
+//                    }
+//                    ft.commitAllowingStateLoss()
+//                }
+                val ft = supportFragmentManager.beginTransaction()
+              //  ft.replace(R.id.fragment_container, PicFragment.newInstance(user.uid , false ))
+                ft.commitAllowingStateLoss()
+            } else {
+                switchToSplashFragment()
+            }
+        }
+
+
+    }
+    private fun switchToSplashFragment() {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, SplashFragment())
+        ft.commit()
+    }
+    override fun onLoginButtonPressed() {
+        launchLoginUI()
+    }
+    private fun launchLoginUI() {
+        // TODO: Build a login intent and startActivityForResult(intent, ...)
+        // For details, see https://firebase.google.com/docs/auth/android/firebaseui#sign_in
+        // Choose authentication providers
+        val providers = arrayListOf(
+
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.PhoneBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build())
+        val loginIntent =  AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.logo)
+                .build()
+// Create and launch sign-in intent
+        startActivityForResult(
+                loginIntent ,
+                RC_SIGN_IN)
+
+    }
+
+
     override fun onOfferSelected(offer: Offer) {
 
        //Toast.makeText(this, offer.toString(), Toast.LENGTH_LONG).show()
@@ -94,6 +182,7 @@ class MainActivity : AppCompatActivity(), MyOffersListFragment.OnOfferSelectedLi
         setContentView(R.layout.activity_main)
         toolbar.title = "Available Offers"
         setSupportActionBar(toolbar)
+        initializeListeners()
 //
 //        fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
